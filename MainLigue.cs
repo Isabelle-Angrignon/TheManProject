2e1e73151b1da254ae270218bbadef2ddc985e0c
Isabelle-Angrignon <isabelle.angrignon@hotmail.com>
2013-12-11 17:03:46 -0500

    pu erreur clisssss

--- MainLigue.cs
+++ MainLigue.cs
@@ -238,10 +238,6 @@ namespace The_Main_Project
             OuvertureTop5();
         }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b88393fbda4ff014429ce4c38287fe1a2154f1ea
         private void Form_League_SizeChanged(object sender, EventArgs e)
         {
             //constater nouvelle size, redimensionne éléments tels dgv en fonction de la nouvelle dimension
@@ -265,9 +261,7 @@ namespace The_Main_Project
         private void TSMI_Match_Click(object sender, EventArgs e)
         {
             OuvrireMatch();
-        }
-
-       
+        }       
 
         private void OuvrireParam()
         {
